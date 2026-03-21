b66e20db9b7c9c3b30ad8710515d3471a8ddbf5c
Zain <zu4312260@gmail.com>
2026-03-21 14:56:36 -0600

    implementing visitor

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -7,27 +7,57 @@ Generator::Generator(node::prog prog)
 
 }
 
+void Generator::gen_expr(const node::expr& expr){
+	struct ExprVisitor {
+		Generator* gen;
 
-std::string Generator::gen_statement(const node::statement) const {
-	
+		void operator()(const node::exprIntLit& expr_int_lit){
+			gen->m_output << "li t0, " << expr_int_lit.int_lit.value.value() << "\n";
+			gen->m_output << "ld t0, 0(sp)";
+			gen->m_output << "add sp, 8\n";
+		}
+		void operator()(const node::exprIdent& expr_iden){
+		//TODO	
+		}
+	};
+	ExprVisitor visitor{.gen = this};
+	std::visit(visitor, expr.var);
 }
 
-std::string Generator::gen_prog() const {
-	std::stringstream output;
-	output << "		.global main\nmain:\n";
+void Generator::gen_statement(const node::statement& stmt){
+	struct StmtVisitor {
+		Generator* gen;
+
+		void operator()(const node::statementReturn& stmt_ret){
+			gen_expr(stmt_ret.expr);
+			gen->m_output << "		li a0, 0\n";
+			gen->m_output << "		li a7, "; //TODO:
+			gen->m_output << "		ecall\n";
+		}
+		void operator()(const node::statementDeclaration& stmt_dec){
+			
+		}
+	};
+		StmtVisitor visitor{.gen = this};
+		std::visit(visitor, stmt.stmt);
+
+}
+
+std::string Generator::gen_prog(){
+	m_output << "		.global main\nmain:\n";
 	
 	for(const node::statement stmt : m_prog.stmts){
-		output << gen_statement(stmt);
+	m_output << gen_statement(stmt);
 	}
 
 
 
 
-	output << "		li a0, 0\n";
-	output << "		li a7, 1\n";
-	output << "		ecall\n";
-	output << "		li a0, 0\n";
-	output << "		li a7, 10\n";
-	output << "		ecall";
-	return output.str();
+	m_output << "		li a0, 0\n";
+	m_output << "		li a7, 1\n";
+	m_output << "		ecall\n";
+	m_output << "		li a0, 0\n";
+	m_output << "		li a7, 10\n";
+	m_output << "		ecall";
+	return m_output.str();
 }
--- src/codegen.h
+++ src/codegen.h
@@ -4,15 +4,19 @@
 #include <iostream>
 #include "parser.h"
 #include "node.h"
+#include <sstream>
 class Generator{
 	public:
 		Generator(node::prog);
 
-		[[nodiscard]] std::string gen_prog () const;
+		[[nodiscard]] std::string gen_prog ();
 
-		[[nodiscard]] std::string gen_statement (const node::statement) const;
+		void gen_statement (const node::statement&);
+
+		void gen_expr(const node::expr&);
 	private:
 		const node::prog m_prog;
+		std::stringstream m_output;
 
 };
 
